45ad8e996b77505726d77b2b48bcb1b9b0edab4b
RGNGIT <vnifedow@bk.ru>
2023-10-28 15:05:34 +0500

    Topics logic

--- src/topic/topic.module.ts
+++ src/topic/topic.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from 'src/sequelize/sequelize.module';
 import { topicProviders } from './providers/topic.providers';
+import {TopicService} from './services/topic.service';
+import {TopicController} from './controllers/topic.controller';
 
 @Module({
   imports: [SequelizeModule],
-  controllers: [],
-  providers: [ ...topicProviders ]
+  controllers: [TopicController],
+  providers: [ TopicService, ...topicProviders ]
 })
 
 export class TopicModule {}
